1dfaa4fb37875a368ed727e433b3447efd6b44b9
Forntoh Thomas <thomasforntoh@gmail.com>
2021-11-06 23:31:39 +0100

    moved files

--- src/index.js
+++ src/index.js
@@ -1,104 +1,88 @@
-import IG from "./api";
+import { create, setHeaderTokens } from "./connection";
+import {
+  uniqueId,
+  getOption,
+  isFunction,
+  transformError,
+  transformResponse,
+} from "./utils.js";
 
-const ig = new IG(process.env.IG_API_KEY, true);
+export default class IG {
+  static transformResponse = transformResponse;
+  static transformError = transformError;
+  static uniqueId = uniqueId;
 
-// Using promises
-ig.login(process.env.IG_USERNAME, process.env.IG_USERNAME)
-  // Response data is automatically
-  // passed to the resolve callback
-  // .then((summary) => {
-  //   console.log("summary:", summary);
-  //   // Once logged in, use the shorthand
-  //   // get(), post(), put() and delete()
-  //   // methods to interact with IG's API
-  //   ig.get("positions").then((positions) => {
-  //     console.log("positions:", positions);
-  //   });
-  // })
-  // Errors are automatically transformed
-  // into a more user friendly format with
-  // the response status and IG error code
-  .catch(console.error);
+  constructor(apiKey, isDemo, options) {
+    this.api = create(apiKey, isDemo);
+    this.defaults = Object.assign(
+      {
+        transformResponse,
+        transformError,
+      },
+      options
+    );
+  }
 
-// Using async await
-// try {
-//   await ig.login(username, password);
-//   const positions = await ig.get("positions");
-//   console.log("positions:", positions);
-// } catch (error) {
-//   console.error(error);
-// }
+  request(method, path, version, config, options) {
+    const transformRes = getOption("transformResponse", options, this.defaults);
+    const transformErr = getOption("transformError", options, this.defaults);
 
-// const express = require("express");
-// const app = express();
-// const port = 3000;
+    let request = this.api.request(
+      Object.assign({}, config, {
+        method,
+        url: path,
+        headers: { Version: version || 1 },
+      })
+    );
 
-// const IG = require("./ig.js");
+    if (isFunction(transformRes)) request = request.then(transformRes);
+    if (isFunction(transformErr)) request = request.catch(transformErr);
 
-// const ig = new IG("f201f8d7ff222f525a9d25a22b889125ec2c63ab");
-// // const ig = new IG("a0b3953d597e2179bdbe0cf4a10ba41f5d51283e", false);
+    return request;
+  }
 
-// app.use(express.urlencoded({ extended: true }));
+  get(path, version, params, options) {
+    return this.request("get", path, version, { params }, options);
+  }
 
-// app.get("/prices", async (req, res) => {
-//   try {
-//     const evening = await ig.prices({
-//       epic: "IX.D.DAX.IFD.IP",
-//       resolution: "MINUTE_5",
-//       from: "2021-11-03T21:00:00",
-//       to: "2021-11-03T21:00:00",
-//     });
+  post(path, version, data, options) {
+    return this.request("post", path, version, { data }, options);
+  }
 
-//     const morning = await ig.prices({
-//       epic: "IX.D.DAX.IFD.IP",
-//       resolution: "MINUTE_5",
-//       from: "2021-11-04T07:00:00",
-//       to: "2021-11-04T07:00:00",
-//     });
+  put(path, version, data, options) {
+    return this.request("put", path, version, { data }, options);
+  }
 
-//     const recent = await ig.prices({
-//       epic: "IX.D.DAX.IFD.IP",
-//       resolution: "MINUTE_5",
-//       from: "2021-11-04T06:00:00",
-//       to: "2021-11-04T06:55:00",
-//     });
+  delete(path, version, data, options) {
+    return this.request("delete", path, version, { data }, options);
+  }
 
-//     const gapEvening = evening[0].closePrice.bid - evening[0].openPrice.bid;
-//     const gapMorning = morning[0].closePrice.bid - morning[0].openPrice.bid;
+  async login(username, password, options) {
+    const processPassword = Promise.resolve(password);
+    return processPassword.then((result) => {
+      return this.post(
+        "session",
+        2,
+        {
+          identifier: username,
+          password: result,
+        },
+        {
+          transformResponse: false,
+        }
+      ).then((response) => {
+        setHeaderTokens(this.api, response);
+        const transformRes = getOption(
+          "transformResponse",
+          options,
+          this.defaults
+        );
+        return isFunction(transformRes) ? transformRes(response) : response;
+      });
+    });
+  }
 
-//     const aM = gapEvening >= 0.0 ? "bull" : "bear";
-//     const bM = gapMorning >= 0.0 ? "bull" : "bear";
-
-//     recent.sort((a, b) => (a.highPrice.bid < b.highPrice.bid ? 1 : -1));
-
-//     const topDist =
-//       morning[0].closePrice.bid -
-//       recent[0].highPrice.bid +
-//       (morning[0].openPrice.bid - morning[0].openPrice.ask);
-//     const bottomDist = morning[0].closePrice.bid - evening[0].openPrice.bid;
-
-//     res.json({
-//       "21:00": { ...evening[0], aM },
-//       "07:00": { ...morning[0], bM },
-//       recentHigh: recent[0],
-//       topDist,
-//       bottomDist,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// app.get("/logout", (req, res) => {
-//   ig.logout()
-//     .then((r) => res.send(r))
-//     .catch((e) => res.send(e));
-// });
-
-// app.listen(port, () => {
-//   ig.login({
-//     identifier: "forntoh",
-//     password: "N@$?MabX84tt6c6S",
-//   });
-//   //   .then((res) => console.log(res));
-// });
+  logout(options) {
+    return this.delete("session", 1, null, options);
+  }
+}
